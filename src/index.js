a278d74d84c6d3d92615c4dce8f1ef21a9e3fd5f
Karol <kpulawski13@gmail.com>
2023-06-27 10:02:21 +0100

    added axios

--- src/index.js
+++ src/index.js
@@ -4,4 +4,6 @@ import laughing from "./assets/happy.svg";
 
 const laughImg = document.querySelector("#laughImg");
 laughImg.src = laughing;
+
+const jokeBtn = docuemtn.getElement;
 console.log(generateJoke());
