f8441f8d3b5b237c649896c0762ef487eddffab7
Karol <kpulawski13@gmail.com>
2023-06-27 09:37:39 +0100

    add image to config

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import generateJoke from "./generateJoke";
 import "./styles/main.scss";
+import laughing from "./assets/happy.svg";
 
+const laughImg = document.querySelector("#laughImg");
+laughImg.src = laughing;
 console.log(generateJoke());
